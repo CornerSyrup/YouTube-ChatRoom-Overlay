a9cf984f63c008e9b356b14e766bae4fd4c6db8d
CornerSyrup <87284079+CornerSyrup@users.noreply.github.com>
2021-08-18 17:14:00 +0000

    :recycle: refactor: Extract to func to reuse

--- overlay.js
+++ overlay.js
@@ -1,22 +1,37 @@
-let videoContainer;
+let videoFrame;
 let chatRoom;
 
 function overlay() {
-    videoContainer = document.querySelector(
-        "video.video-stream.html5-main-video"
-    )?.parentElement;
+    videoFrame = document.querySelector("video.video-stream.html5-main-video");
     chatRoom = document.querySelector("ytd-live-chat-frame#chat");
 
-    if (videoContainer == null) {
+    if (videoFrame == null) {
         console.warn("[YT Chat Overlay] Video container not found...");
         return;
     } else if (chatRoom == null) {
         console.warn("[YT Chat Overlay] Chat room not found...");
         return;
-    } else {
-        console.info("[YT Chat Overlay] Chat room has been overlay to video.");
-        videoContainer.appendChild(chatRoom);
     }
+
+    console.info("[YT Chat Overlay] Chat room has been overlay to video.");
+    videoFrame.parentElement.appendChild(chatRoom);
+
+    resizeChatRoom(videoFrame);
 }
+
+function resizeChatRoom(frame) {
+    console.log("[YTCO] Start of resize...");
+
+    chatRoom.style.padding = isFullScreen() ? "7vh 0 8vh" : "0";
+
+    console.log("[YTCO] End of resize...");
+}
+
+isFullScreen = () => window.screen.height <= videoHeight();
+
+videoHeight = () =>
+    Number.parseInt(
+        window.getComputedStyle(videoFrame, null).getPropertyValue("height")
+    );
 
 setTimeout(overlay, 1 * 1000);
